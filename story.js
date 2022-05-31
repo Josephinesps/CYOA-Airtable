d7bf112c17e3adf4d03d99202760dfddaf4ce025
Josephinesps <98600503+Josephinesps@users.noreply.github.com>
2022-05-31 13:59:47 -0400

    Update story.js

--- story.js
+++ story.js
@@ -2,7 +2,7 @@
 
 window.onload = start;
 // Change this to match ID in your AirTable.
-const OPENING_SCENE_ID = 'rec7fFP6hHNgDTQuQ';
+const OPENING_SCENE_ID = 'recqPkVvAh7Qx6oPV';
 
 function start() {
     setup();
@@ -14,8 +14,8 @@ function getScene(record_id) {
   // Replace with your own AirTable API key.
   // Normally, you will want to keep this private.
   // This key will only be good for a couple of days.
-  const key = 'keyJLBdH3ktq1BJ8L';
-  const base_url = 'appHHB0tdKOoiGYPh'; 
+  const key = 'keyOi33iBMD2JDiLK';
+  const base_url = 'appx2CmhTSiWQ1zwi'; 
   // Alter this to match your own AirTable base.
   // URL format is
   // https://api.airtable.com/v0/<BASE_ID>/<TABLE_NAME>/<RECORD_ID>?api_key=<YOUR_API_KEY>
